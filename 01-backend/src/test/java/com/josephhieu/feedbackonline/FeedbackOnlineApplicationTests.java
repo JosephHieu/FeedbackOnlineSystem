4e8a6433c05a0652a727ee7f84b45d0898994cf1
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2026-03-30 16:25:54 +0700

    Add @TestPropertySource to FeedbackOnlineApplicationTests

--- 01-backend/src/test/java/com/josephhieu/feedbackonline/FeedbackOnlineApplicationTests.java
+++ 01-backend/src/test/java/com/josephhieu/feedbackonline/FeedbackOnlineApplicationTests.java
@@ -2,12 +2,18 @@ package com.josephhieu.feedbackonline;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.jpa.hibernate.ddl-auto=update",
+        "spring.mail.host=localhost"
+})
 class FeedbackOnlineApplicationTests {
 
     @Test
     void contextLoads() {
+        
     }
 
 }
